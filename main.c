329f612bc994844d51ff7746005db5bdca98d320
m1cha1s <msulej04@gmail.com>
2024-05-19 21:30:15 +0200

    Texture rendering is working!!!

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ https://antongerdelan.net/opengl/d3d11.html
 */
 
 #include <stdbool.h>
+#include <float.h>
 
 #define WIN32_LEAN_AND_MEAN
 #define COBJMACROS
@@ -14,6 +15,8 @@ https://antongerdelan.net/opengl/d3d11.html
 #include <dxgi.h>
 #include <d3dcompiler.h>
 #include <assert.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // GLOBALS
 
@@ -116,7 +119,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL,
         NULL,
         //D3D_COMPILE_STANDARD_FILE_INCLUDE,
-        "vs_main",
+        "vsMain",
         "vs_5_0",
         flags,
         0,
@@ -138,7 +141,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL,
         NULL,
         //D3D_COMPILE_STANDARD_FILE_INCLUDE,
-        "ps_main",
+        "psMain",
         "ps_5_0",
         flags,
         0,
@@ -178,12 +181,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     
     ID3D11InputLayout* inputLayoutPtr = NULL;
     D3D11_INPUT_ELEMENT_DESC inputElementDesc[] = {
-        { "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        /*
-        { "COL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "NOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEX", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, ?
-        */
+        { "POSITION",  0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "UV", 0, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
     
     hr = ID3D11Device_CreateInputLayout(
@@ -198,13 +197,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     
     // Create vertex buffer
     float vertexDataArray[] = {
-         0.0f,  0.5f,  0.0f, // top
-         0.5f, -0.5f,  0.0f, // bottom-right
-        -0.5f, -0.5f,  0.0f, // bottom-left
+        -0.5f,  0.5f,  0.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f, -0.5f,  0.0f,  1.0f,  1.0f,
     };
-    UINT vertexStride = 3*sizeof(float);
+    UINT vertexStride = 5*sizeof(float);
     UINT vertexOffset = 0;
-    UINT vertexCount  = 3;
+    UINT vertexCount  = 6;
     
     ID3D11Buffer* vertexBufferPtr = NULL;
     
@@ -223,6 +225,73 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     );
     assert(SUCCEEDED(hr));
     
+    // Load and set texture.
+    
+    int x,y,n;
+    unsigned char *bitmap = stbi_load("res/aaa.png", &x, &y, &n, 4);
+    assert(bitmap); // Make sure the bitmap has been loaded.
+    
+    ID3D11Texture2D* texture = NULL;
+    D3D11_TEXTURE2D_DESC textureDesc = { 0 };
+    textureDesc.Width = x;
+    textureDesc.Height = y;
+    textureDesc.MipLevels = 1;
+    textureDesc.ArraySize = 1;
+    textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+    textureDesc.SampleDesc.Count = 1;
+    textureDesc.SampleDesc.Quality = 0;
+    textureDesc.Usage = D3D11_USAGE_DYNAMIC;
+    textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+    textureDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    // textureDesc.MiscFlags = 
+    
+    D3D11_SUBRESOURCE_DATA srTex = { 0 };
+    srTex.pSysMem = bitmap;
+    srTex.SysMemPitch = x*4;
+    
+    hr = ID3D11Device_CreateTexture2D(
+        devicePtr,
+        &textureDesc,
+        &srTex,
+        &texture
+    );
+    assert(SUCCEEDED(hr));
+    
+    stbi_image_free(bitmap);
+    
+    ID3D11ShaderResourceView *textureShaderResViewPtr = NULL;
+    
+    hr = ID3D11Device_CreateShaderResourceView(
+        devicePtr,
+        (ID3D11Resource *)texture,
+        NULL,
+        &textureShaderResViewPtr
+    );
+    assert(SUCCEEDED(hr));
+    
+    D3D11_SAMPLER_DESC textureSamplerDesc = { 0 };
+    textureSamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    textureSamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+    textureSamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+    textureSamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+    textureSamplerDesc.MinLOD = -FLT_MAX;
+    textureSamplerDesc.MaxLOD = FLT_MAX;
+    textureSamplerDesc.MipLODBias = 0.0f;
+    textureSamplerDesc.MaxAnisotropy = 1;
+    textureSamplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+    textureSamplerDesc.BorderColor[0] = 1.0f;
+    textureSamplerDesc.BorderColor[1] = 1.0f;
+    textureSamplerDesc.BorderColor[2] = 1.0f;
+    textureSamplerDesc.BorderColor[3] = 1.0f;
+    
+    ID3D11SamplerState* samplerStatePtr = NULL;
+    
+    ID3D11Device_CreateSamplerState(
+        devicePtr,
+        &textureSamplerDesc,
+        &samplerStatePtr
+    );
+    
     ShowWindow(hwnd, nCmdShow);
     
     MSG msg = { 0 };
@@ -237,21 +306,21 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         vertexDataArray[0] += 0.01f;
         if (vertexDataArray[0]>=1.0f) vertexDataArray[0] = -1.0f;
         
-        D3D11_MAPPED_SUBRESOURCE resource = { 0 };
-        ID3D11DeviceContext_Map(
-            deviceContextPtr,
-            (ID3D11Resource *)vertexBufferPtr,
-            0,
-            D3D11_MAP_WRITE_DISCARD,
-            0,
-            &resource
-        );
-        memcpy(resource.pData, vertexDataArray, sizeof(vertexDataArray));
-        ID3D11DeviceContext_Unmap(
-            deviceContextPtr,
-            (ID3D11Resource *)vertexBufferPtr,
-            0,
-        );
+        // D3D11_MAPPED_SUBRESOURCE resource = { 0 };
+        // ID3D11DeviceContext_Map(
+        //     deviceContextPtr,
+        //     (ID3D11Resource *)vertexBufferPtr,
+        //     0,
+        //     D3D11_MAP_WRITE_DISCARD,
+        //     0,
+        //     &resource
+        // );
+        // memcpy(resource.pData, vertexDataArray, sizeof(vertexDataArray));
+        // ID3D11DeviceContext_Unmap(
+        //     deviceContextPtr,
+        //     (ID3D11Resource *)vertexBufferPtr,
+        //     0,
+        // );
         
         // Clear background
         float backgroundColor[4] = {
@@ -288,6 +357,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
             deviceContextPtr,
             inputLayoutPtr
         );
+        
+        ID3D11DeviceContext_PSSetShaderResources(
+            deviceContextPtr,
+            0, 1,
+            &textureShaderResViewPtr
+        );
+        ID3D11DeviceContext_PSSetSamplers(
+            deviceContextPtr,
+            0, 1,
+            &samplerStatePtr
+        );
+        
         ID3D11DeviceContext_IASetVertexBuffers(
             deviceContextPtr,
             0,
--- shaders.hlsl
+++ shaders.hlsl
@@ -1,31 +1,30 @@
-struct vs_in {
-    float3 pos : POS;
+struct vsIn {
+    float3 Position : POSITION;
+    float2 TextureUV : UV;
 };
 
-struct vs_out {
-    float4 screenPos : SV_POSITION;
-    float4 uv : LOLIMASEMANTIC;
+struct vsOut {
+    float2 TextureUV : UV;
+    float4 Position  : SV_POSITION;
 };
 
-vs_out vs_main(vs_in input) {
-    vs_out output = (vs_out)0;
-    output.screenPos = float4(input.pos, 1.0);
-    output.uv = float4(input.pos, 1.0);
+vsOut vsMain(vsIn input) {
+    vsOut output = (vsOut)0;
+    
+    output.Position = float4(input.Position, 1.0);
+    output.TextureUV = input.TextureUV;
+    
     return output;
 }
 
+Texture2D MeshTexture;
+SamplerState MeshTextureSampler;
+
+struct psIn {
+    float2 TextureUV : UV;
+};
+
 // Returns RGBA
-float4 ps_main(vs_out input) : SV_TARGET {
-    // return float4( 1.0, 0.0, 1.0, 1.0 );
-    float u = (1.0+input.uv.x)/2.0;
-    float v = (1.0+input.uv.y)/2.0;
-    
-    return float4(
-        u, 
-        v, 
-        // 0.1*u+0.7*v, 
-        0.0,
-        1.0 
-    );
-    // return input.uv;
+float4 psMain(psIn input) : SV_TARGET {
+    return MeshTexture.Sample(MeshTextureSampler, input.TextureUV);
 }
